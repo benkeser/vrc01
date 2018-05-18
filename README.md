0f46cbb3887d7d2247b7783bb63aee2793c1cec5
Benkeser <benkeser@emory.edu>
2018-05-18 15:57:07 -0400

    add first files, update README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # vrc01
-Code for "HIV-1 Sequence Predictors of VRC01 Neutralization Sensitivity"
+> Code repository for "Prediction of VRC01 neutralization sensitivity by HIV-1 gp160 sequence features"
+
+**Authors:** Craig A. Magaret, David C. Benkeser, Brian D. Williamson, Bhavesh R. Borate, Lindsay N. Carpp, Ivelin S. Georgiev, Ian Setliff, Adam S. Dingens, Noah Simon, Marco Carone, David Montefiori, Galit Alter, Wen-Han Yu, Michal Juraska, Paul T. Edlefsen, Shelly Karuna, Nyaradzo M. Mgodi, Srilatha Edugupanti Peter B. Gilbert 
+
+-----
+
+## Description 
+
+This repository contains code to reproduce the analyses of "Prediction of VRC01 neutralization sensitivity by HIV-1 gp160 sequence features" by Margaret, Williamson, Benkeser, et al (2018). This README provides a road map to the code available in the repository. The analysis was implemented in the freely available [`R`](https://cran.r-project.org/web/checks/check_results_drtmle.html) programming language. 
+
+-----
+
+## Data directory
+
+The `data` directory contains two .csv files corresponding to the two separate data sets that were analyzed in the paper. These may be downloaded and locally read into `R` via `read.csv`, or, alternatively can be sourced directly from GitHub as follows.
+
+```r
+# RCurl can read directly from GitHub
+library(RCurl)
+# load data set 1
+data1 <- read.csv(text = getURL("https://raw.githubusercontent.com/benkeser/vrc01/master/data/data1.csv"), header = TRUE)
+# load data set 2
+data2 <- read.csv(text = getURL("https://raw.githubusercontent.com/benkeser/vrc01/master/data/data2.csv"), header = TRUE)
+```
+
+-----
+
+## Code directory
+
+We have separated our code into two folders based on analysis objective, where the two objectives are restated here: 
+
+1. Model selection -- to develop a best model or best few models for predicting TZM-bl neutralization sensitivity to VRC01 and advance the predicted values from this model or models as proteomic resistance scores for use in the primary AA sequence sieve analysis.
+2. Feature selection -- to rank AA sequence features by their importance for predicting TZM-bl neutralization sensitivity to VRC01 and select the most important features to advance to the primary sieve analysis.
+
+Within each folder, we have adopted a naming convention for the `R` scripts therein. Files starting with 00- are considered preliminary files, that must be `source`'ed in to `R` to execute subsequent files. For example, `code/objective1/00-prelims.R` is sourced by `code/objective1/01-run_superlearner.R`. 
+
+-----
+
+## Issues
+
+If you encounter any bugs or have any specific questions about the analysis, please
+[file an issue](https://github.com/benkeser/vrc01/issues).
+
---
+++ code/objective1/01-run_superlearner.R
@@ -0,0 +1,159 @@
+#----------------------------------------------------------------------
+# This script performs the super learner analysis for each of the 
+# outcomes. Note that this the execution of this script is fairly 
+# time intensive. The implementation can be sped up through multicore
+# parallelization (see ?future::plan)
+#----------------------------------------------------------------------
+
+# load libraries
+library(data.table)
+library(SuperLearner)
+library(future)
+library(cvma)
+library(glmnet)
+library(xgboost)
+library(e1071)
+# note cvma may be installed from GitHub via
+# devtools::install_github("benkeser/cvma")
+library(cvma)
+# define directory to source makeDataAndFunctions
+codeDir <- "./code/objective1/"
+# define director to save results
+outDir <- "."
+# run makeDataAndFunctions.R script
+source(paste0(codeDir, "00-prelims.R"))
+
+# make future plan
+# plan(multicore)
+plan(sequential)
+
+#----------------------------------------
+# Analysis on data set 1
+#----------------------------------------
+Y.slope.mod <- Y.slope
+
+# identifier of obs with complete data
+idx <- !is.na(Y.80) & !is.na(Y.slope) & !(Y.slope == Inf)
+idx.mod <- !is.na(Y.80) & !is.na(Y.slope)
+# make a list of the relevant continuous outcomes
+outcomes_list <- list(data.frame(ic50 = Y),
+                      data.frame(ic80 = Y.80[!is.na(Y.80)]),
+                      data.frame(slope_mod = Y.slope.mod[!is.na(Y.slope.mod)])
+                    )
+# make list of the relevant predictors
+predictors_list <- list(X, X[!is.na(Y.80), ], X[!is.na(Y.slope.mod),])
+
+# fit each outcome with super learner from cvma package using appropriate predictors
+for(i in 1:length(outcomes_list)){
+	this_name <- colnames(outcomes_list[[i]])
+	cat(paste0("\n \n \n \n \n Fitting ", this_name, " on set 1. \n \n \n \n \n"))
+	system.time(fit <- cvma(
+  		Y = outcomes_list[[i]], X = predictors_list[[i]], 
+  		learners = SL.library.continuous, V = 10,
+      return_control = list(outer_weight = TRUE,
+                            outer_sl = TRUE, inner_sl = TRUE, 
+                            all_y = TRUE, all_learner_assoc = TRUE,
+                            all_learner_fits = FALSE)))
+	if(length(this_name) > 1){
+		this_name <- ifelse("slope_mod" %in% this_name, "all_mod","all")
+	}
+	save(fit, file = paste0(outDir,"fit_",this_name,"_set1_v11_newest.RData"))
+}
+
+# make a list of the relevant dichotomous outcomes
+outcomes_list <- list(data.frame(cens = Y.cens[!is.na(Y.cens)]),
+                      data.frame(sens.resis = Y.sens.resis[!is.na(Y.sens.resis)]))
+
+# make list of the relevant predictors
+predictors_list <- list(X[!is.na(Y.cens), ], X[!is.na(Y.sens.resis), ])
+
+for(i in 1:length(outcomes_list)){
+	this_name <- colnames(outcomes_list[[i]])
+	cat(paste0("\n \n \n \n \n Fitting ", this_name, " on set 1. \n \n \n \n \n \n"))
+	system.time(fit <- cvma(
+  		Y = outcomes_list[[i]], X = predictors_list[[i]], 
+  		learners = SL.library.binary, V = 10, 
+  		 sl_control = list(ensemble_fn = "ensemble_linear",
+                                   optim_risk_fn = "optim_risk_sl_nloglik",
+                                   weight_fn = "weight_sl_convex",
+                                   cv_risk_fn = "cv_risk_sl_auc",
+                                   family = binomial(),
+                                   alpha = 0.05),
+                y_weight_control = list(ensemble_fn = "ensemble_linear",
+                                  weight_fn = "weight_y_01",
+                                  optim_risk_fn = "optim_risk_y_auc",
+                                  cv_risk_fn = "cv_risk_y_auc",
+                                  alpha = 0.05),
+                return_control = list(outer_weight = TRUE,
+                                      outer_sl = TRUE, inner_sl = TRUE, 
+                                      all_y = TRUE, all_learner_assoc = TRUE,
+                                      all_learner_fits = FALSE)))
+	save(fit, file = paste0(outDir, "fit_",
+	                        colnames(outcomes_list[[i]]),"_set1_v11_newest.RData"))
+}
+
+#----------------------------------------
+# Analysis on data set 2
+#----------------------------------------
+Y2.slope.mod <- Y2.slope
+
+# identifier of obs with complete data
+idx <- !is.na(Y2.80) & !is.na(Y2.slope) & !(Y2.slope == Inf)
+idx.mod <- !is.na(Y2.80) & !is.na(Y2.slope)
+# make a list of the relevant continuous outcomes
+outcomes_list <- list(data.frame(ic50 = Y2),
+                      data.frame(ic80 = Y2.80[!is.na(Y2.80)]),
+                      data.frame(slope_mod = Y2.slope.mod[!is.na(Y2.slope.mod)])
+                    )
+# make list of the relevant predictors
+predictors_list <- list(X2, X2[!is.na(Y2.80), ], X2[!is.na(Y2.slope.mod),])
+
+for(i in 1:length(outcomes_list)){
+  this_name <- colnames(outcomes_list[[i]])
+	cat(paste0("\n \n \n \n \n Fitting ", this_name, " on set 2. \n \n \n \n \n"))
+	system.time(fit <- cvma(
+  		Y = outcomes_list[[i]], X = predictors_list[[i]], 
+  		learners = SL.library.continuous, V = 10,
+      return_control = list(outer_weight = TRUE,
+                                      outer_sl = TRUE, inner_sl = TRUE, 
+                                      all_y = TRUE, all_learner_assoc = TRUE,
+                                      all_learner_fits = FALSE)))
+	this_name <- colnames(outcomes_list[[i]])
+	if(length(this_name) > 1){
+		this_name <- ifelse("slope_mod" %in% this_name, "all_mod","all")
+	}
+	save(fit, file = paste0(outDir,
+	                        this_name,"_set2_v11_newest.RData"))
+}
+
+# make a list of the relevant dichotomous outcomes
+outcomes_list <- list(data.frame(cens = Y2.cens[!is.na(Y2.cens)]),
+                      data.frame(sens.resis = Y2.sens.resis[!is.na(Y2.sens.resis)]))
+
+# make list of the relevant predictors
+predictors_list <- list(X2[!is.na(Y2.cens), ], X2[!is.na(Y2.sens.resis), ])
+
+for(i in 1:length(outcomes_list)){
+	this_name <- colnames(outcomes_list[[i]])
+	cat(paste0("\n \n \n \n \n Fitting ", this_name, " on set 2. \n \n \n \n \n"))
+	system.time(fit <- cvma(
+  		Y = outcomes_list[[i]], X = predictors_list[[i]], 
+  		learners = SL.library.binary, V = 10,
+  		 sl_control = list(ensemble_fn = "ensemble_linear",
+                                   optim_risk_fn = "optim_risk_sl_nloglik",
+                                   weight_fn = "weight_sl_convex",
+                                   cv_risk_fn = "cv_risk_sl_auc",
+                                   family = binomial(),
+                                   alpha = 0.05),
+                y_weight_control = list(ensemble_fn = "ensemble_linear",
+                                  weight_fn = "weight_y_01",
+                                  optim_risk_fn = "optim_risk_y_auc",
+                                  cv_risk_fn = "cv_risk_y_auc",
+                                  alpha = 0.05),
+                return_control = list(outer_weight = TRUE,
+                                      outer_sl = TRUE, inner_sl = TRUE, 
+                                      all_y = TRUE, all_learner_assoc = TRUE,
+                                      all_learner_fits = FALSE)))
+	save(fit, file = paste0(outDir,
+	                        colnames(outcomes_list[[i]]),"_set2_v11_newest.RData"))
+}
